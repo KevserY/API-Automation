5174aa741ef21e1a9198771072a1d470ab705cb5
Kevser Yazici <mailkevseryazici@gmail.com>
2024-02-20 12:33:59 +0300

    v5_BaseURI

---
+++ src/test/java/tests/C18_baseUrlJsonPlaceDelete.java
@@ -0,0 +1,27 @@
+package tests;
+
+import baseUrl.BaseUrlJsonPlaceUrl;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C18_baseUrlJsonPlaceDelete extends BaseUrlJsonPlaceUrl {
+
+// https://jsonplaceholder.typicode.com/posts/50 endpointine
+//   bir DELETE request gonderdigimizde donen response'un
+//   status code'unun 200 oldugunu
+//   ve response body'sinin null oldugunu test edin
+
+    @Test
+    public void deleteTesti(){
+        specJsonPlaceHolder.pathParams("pp1", "post", "pp2", "51");
+
+        Response response = given().when().spec(specJsonPlaceHolder).delete("{pp1}/{pp2}");
+
+        response.then().assertThat()
+                .statusCode(200)
+                .body("title", Matchers.nullValue());
+    }
+}
